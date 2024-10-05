4db89a52c1979d78b4ec390631404284fe9355d8
allenabraham777 <allenabraham777@gmail.com>
2024-10-05 22:00:23 +0530

    package - :zap: create dicom utils

---
+++ packages/dicom/index.js
@@ -0,0 +1,32 @@
+// packages/dicom-utils/index.js
+
+import * as dicomParser from "dicom-parser";
+import dcmjs from "dcmjs";
+
+/**
+ * Parses a DICOM file and returns the dataset.
+ * @param {File} file - The DICOM file to parse.
+ * @returns {Promise<Object>} - The parsed DICOM dataset.
+ */
+export function parseDicomFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const arrayBuffer = event.target.result;
+      const byteArray = new Uint8Array(arrayBuffer);
+      try {
+        const dataSet = dicomParser.parseDicom(byteArray);
+        resolve(dataSet);
+      } catch (error) {
+        reject(error);
+      }
+    };
+
+    reader.onerror = function () {
+      reject(reader.error);
+    };
+
+    reader.readAsArrayBuffer(file);
+  });
+}
